05f47d539ab89115a4ed9b4f8044715a29968d46
alex <alex.goyer@epitech.eu>
2022-08-17 15:08:13 +0200

    Add beginning of a chart component

--- src/GraphList.js
+++ src/GraphList.js
@@ -1,14 +1,43 @@
-import React, { useRef } from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import CircularProgress from '@mui/material/CircularProgress';
+import React, { useState, useEffect } from 'react';
+import LineChart from './LineChart';
 
 export default function GraphList() {
-
+    const UserData = [
+        {
+          id: 1,
+          year: 2016,
+          userGain: 80000,
+          userLost: 823,
+        },
+        {
+          id: 2,
+          year: 2017,
+          userGain: 45677,
+          userLost: 345,
+        },
+        {
+          id: 3,
+          year: 2018,
+          userGain: 78888,
+          userLost: 555,
+        },
+        {
+          id: 4,
+          year: 2019,
+          userGain: 90000,
+          userLost: 4555,
+        },
+        {
+          id: 5,
+          year: 2020,
+          userGain: 4300,
+          userLost: 234,
+        },
+      ];
+      
   return (
-      <div>Hello</div>
+      <div>
+          <LineChart baseData={UserData}/>
+      </div>
   );
 }
---
+++ src/LineChart.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Line } from "react-chartjs-2";
+import CircularProgress from '@mui/material/CircularProgress';
+import { Chart as ChartJS } from "chart.js/auto";
+
+export default function LineChart({baseData}) {
+
+    const [chartData, setChartData] = useState({
+        labels: [],
+        datasets: [],
+    });
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                setChartData({
+                    labels: baseData.map((data) => data.year),
+                    datasets: [
+                        {
+                            label: "Users Gained",
+                            data: baseData.map((data) => data.userGain),
+                            backgroundColor: [
+                                "rgba(75,192,192,1)",
+                                "#ecf0f1",
+                                "#50AF95",
+                                "#f3ba2f",
+                                "#2a71d0",
+                            ],
+                            borderColor: "black",
+                            borderWidth: 2,
+                        },
+                    ],
+                });
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        loadData();
+    }, [baseData])
+      
+    return (
+        <div>
+            {chartData.labels.length === 0
+                ? <CircularProgress />
+                : <Line data={chartData} />}
+
+        </div>
+    );
+}
